4f3b3d40b6c4fd43ba43c39323ed8b7f5116faf9
Vern Burton <me@vernburton.com>
2020-05-30 09:53:15 -0500

    Distify chef executable to cinc
    - Rename relavant files
    - Also rename chef-analyze to cinc-analyze
    - Run go generate properly for cinc and cinc-analyze

---
+++ components/main-cinc-wrapper/cmd/env.go
@@ -0,0 +1,56 @@
+//
+// Copyright 2020 Chef Software, Inc.
+//
+// Author: Marc A. Paradise <marc.paradise@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"gitlab.com/cinc-project/upstream/chef-workstation/components/main-cinc-wrapper/dist"
+	"gitlab.com/cinc-project/upstream/chef-workstation/components/main-cinc-wrapper/platform-lib"
+	"github.com/spf13/cobra"
+)
+
+var envCmd = &cobra.Command{
+	Use:                "env",
+	Short:              "Prints environment variables used by %s",
+	DisableFlagParsing: true,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) >= 1 {
+			cmd.Help()
+			os.Exit(0)
+		}
+		ReturnEnvironment()
+		return nil
+	},
+}
+
+func ReturnEnvironment() {
+	err := platform_lib.RunEnvironment()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "ERROR:", err.Error())
+		os.Exit(4)
+	}
+	os.Exit(0)
+}
+
+func init() {
+	envCmd.Short = fmt.Sprintf(envCmd.Short, dist.WorkstationProduct)
+	RootCmd.AddCommand(envCmd)
+}
