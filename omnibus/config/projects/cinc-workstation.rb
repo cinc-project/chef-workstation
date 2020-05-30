86e2a92bedd3fce9761dcaddbdd2357674da53b3
Vern Burton <me@vernburton.com>
2020-05-30 09:53:15 -0500

    Distify chef executable to cinc
    - Rename relavant files
    - Also rename chef-analyze to cinc-analyze
    - Run go generate properly for cinc and cinc-analyze

--- omnibus/config/projects/cinc-workstation.rb
+++ omnibus/config/projects/cinc-workstation.rb
@@ -99,9 +99,9 @@ dependency "ruby-cleanup"
 dependency "more-ruby-cleanup"
 
 dependency "go"
-dependency "main-chef-wrapper"
+dependency "main-cinc-wrapper"
 dependency "chef-automate-collect"
-dependency "chef-analyze"
+dependency "cinc-analyze"
 # removes the go language installed at embedded/go
 dependency "go-uninstall"
 
