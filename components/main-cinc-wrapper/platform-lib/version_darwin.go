0ca14f461d9f1cf6126f53aa0c39f8d2b4ecfe22
Lance Albertson <lance@osuosl.org>
2022-04-06 14:14:57 -0700

    Use cinc paths
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- components/main-cinc-wrapper/platform-lib/version_darwin.go
+++ components/main-cinc-wrapper/platform-lib/version_darwin.go
@@ -146,7 +146,7 @@ func ExpectedOmnibusRoot() string {
 
 func UnmarshallRubyEnv() map[string]interface{} {
 	home, err := os.UserHomeDir()
-	filepath := path.Join(home, ".chef/ruby-env.json")
+	filepath := path.Join(home, ".cinc/ruby-env.json")
 	jsonFile, err := os.Open(filepath)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "ERROR:", err.Error())
@@ -175,7 +175,7 @@ func MatchVersions() bool {
 func EnvDoc() (string, map[string]interface{}, bool, bool) {
 	home, err := os.UserHomeDir()
 	WorkstationVersion := componentVersion("build_version")
-	filepath := path.Join(home, ".chef/ruby-env.json")
+	filepath := path.Join(home, ".cinc/ruby-env.json")
 	jsonFile, err := os.Open(filepath)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "ERROR:", err.Error())
